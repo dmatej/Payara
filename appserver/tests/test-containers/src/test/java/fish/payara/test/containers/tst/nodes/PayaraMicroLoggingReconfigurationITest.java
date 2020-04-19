e4420e4f3dd95da1bb2eced2d202c974cbb1437d
David Matejcek <david.matejcek@payara.fish>
2020-04-19 16:52:09 +0200

    PAYARA-4176 More stable PayaraMicroLoggingReconfigurationITest
    - depends on CUSTCOM-130 - we need PAYARA_ARGS
    - depends on CUSTCOM-55
      - should fix CUSTCOM-134
      - UnsatisfiedDependencyException: There was no object available for
        injection at SystemInjecteeImpl(requiredType=LogManagerService ...

--- appserver/tests/test-containers/src/test/java/fish/payara/test/containers/tst/nodes/PayaraMicroLoggingReconfigurationITest.java
+++ appserver/tests/test-containers/src/test/java/fish/payara/test/containers/tst/nodes/PayaraMicroLoggingReconfigurationITest.java
@@ -34,9 +34,12 @@
 
 package fish.payara.test.containers.tst.nodes;
 
+import com.github.dockerjava.api.model.ContainerNetwork;
+
 import fish.payara.test.containers.tools.container.DasCfg;
 import fish.payara.test.containers.tools.container.PayaraServerContainer;
 import fish.payara.test.containers.tools.container.TestablePayaraPort;
+import fish.payara.test.containers.tools.junit.WaitForExecutable;
 import fish.payara.test.containers.tools.log4j.EventCollectorAppender;
 import fish.payara.test.containers.tools.properties.Properties;
 import fish.payara.test.containers.tools.rs.RestClientCache;
@@ -45,6 +48,7 @@ import fish.payara.test.containers.tst.log.war.LoggingRestEndpoint;
 import java.io.File;
 import java.time.Duration;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Set;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -56,6 +60,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.apache.log4j.spi.LoggingEvent;
+import org.hamcrest.core.StringContains;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.exporter.ZipExporter;
@@ -86,11 +91,13 @@ public class PayaraMicroLoggingReconfigurationITest {
     private static final Logger LOG_DAS = LoggerFactory.getLogger("DAS");
     private static final Logger LOG_MICRO = LoggerFactory.getLogger("MIC");
 
+    private static final String WEBAPP_NAME = PayaraMicroLoggingReconfigurationITest.class.getSimpleName() + "WebApp";
     private static final Properties TEST_CFG = new Properties("test.properties");
     private static final DasCfg CFG_DAS = new DasCfg("hostname-das", TEST_CFG.getString("docker.payara.tag"));
     private static final DasCfg CFG_MIC = new DasCfg("hostname-micro", TEST_CFG.getString("docker.payara.tag"));
     private static final Class<LoggingRestEndpoint> APP_CLASS = LoggingRestEndpoint.class;
 
+    // FIXME: filtering messages
     private static final Set<String> MESSAGES_FOR_FILTER = Arrays
         .stream(new String[] {"STDERR:   akjaskjf" //
         }).collect(Collectors.toSet());
@@ -116,14 +123,10 @@ public class PayaraMicroLoggingReconfigurationITest {
     private final PayaraServerContainer micro = new PayaraServerContainer("payara/micro:" + CFG_DAS.getVersion(), CFG_MIC) //
         .withNetwork(NET).withNetworkMode("bridge").withNetworkAliases(CFG_MIC.getHost()) //
         .withExposedPorts(TestablePayaraPort.getMicroPortValues()).withLogConsumer(new Slf4jLogConsumer(LOG_MICRO))
-        .withCreateContainerCmdModifier(cmd -> {
-            cmd.withHostName(CFG_MIC.getHost());
-        })
         .withFileSystemBind(warFileOnHost.getAbsolutePath(), warFileInMicro.getAbsolutePath())
-        .withCommand("--clustermode", "domain:hostname-das:4900",
-            "--hzPublicAddress", CFG_MIC.getHost(),
-            "--name", "MicroTest", "--deploy", warFileInMicro.getAbsolutePath(), "--contextRoot",
-            "/logging")
+        .withEnv("PAYARA_ARGS", "--clustermode domain:" + CFG_DAS.getHost() + ":4900"
+            + " --hzPublicAddress " + CFG_MIC.getHost()
+            + " --name MicroTest --deploy " + warFileInMicro.getAbsolutePath() + " --contextRoot /logging")
         .waitingFor(Wait.forLogMessage(".*Payara Micro.+ ready.+\\n", 1).withStartupTimeout(Duration.ofSeconds(30L)));
 
     private EventCollectorAppender microLog;
@@ -135,10 +138,9 @@ public class PayaraMicroLoggingReconfigurationITest {
             .addAsWebResource(EmptyAsset.INSTANCE, "WEB-INF/beans.xml")
         ;
         LOG.info(war.toString(true));
-        warFileOnHost = new File(TEST_CFG.getFile("build.directory"),
-            PayaraMicroLoggingReconfigurationITest.class.getSimpleName() + ".war");
+        warFileOnHost = new File(TEST_CFG.getFile("build.directory"), WEBAPP_NAME + ".war");
         war.as(ZipExporter.class).exportTo(warFileOnHost, true);
-        warFileInMicro = new File("/", warFileOnHost.getName());
+        warFileInMicro = new File("/opt/payara/", warFileOnHost.getName());
     }
 
 
@@ -172,17 +174,20 @@ public class PayaraMicroLoggingReconfigurationITest {
 
 
     @Test
-    public void testSetLogLevels() throws Exception {
-        das.asAdmin("list-hazelcast-cluster-members");
-        // TODO assert
-
+    public void testSetLogLevels() throws Throwable {
+        WaitForExecutable.waitFor(
+            () -> assertThat(das.asAdmin("list-hazelcast-cluster-members"), StringContains.containsString(WEBAPP_NAME)),
+            15000L);
         final Client client = RS_CLIENTS.getNonPreemptiveClient(true, null, null);
         final WebTarget target = client.target(micro.getHttpUrl().toURI()).path("logging").path("sample");
         final Response response = target.request().post(Entity.entity("", MediaType.TEXT_PLAIN_TYPE));
         // TODO: check response and log
 
-        final String microIpAddress = micro.getContainerInfo().getNetworkSettings().getNetworks().values().iterator()
-            .next().getIpAddress();
+        final Collection<ContainerNetwork> microIpAddresses = micro.getContainerInfo().getNetworkSettings()
+            .getNetworks().values();
+        LOG.info("Micro network addresses: {}",
+            microIpAddresses.stream().map(ContainerNetwork::getIpAddress).collect(Collectors.toList()));
+        final String microIpAddress = microIpAddresses.iterator().next().getIpAddress();
         das.asAdmin("send-asadmin-command", "--explicitTarget=" + microIpAddress + ":6900:MicroTest", //
             "--logOutput=true",
             "--command=set-log-levels", LoggingRestEndpoint.class.getName() + "=FINE");
